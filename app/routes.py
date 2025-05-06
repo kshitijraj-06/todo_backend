639a71feb5c2c198fddd212bf4a7f51682e746f0
kshit <kshitij6102004@gmail.com>
2025-05-06 18:51:36 +0530

    changed error

--- app/routes.py
+++ app/routes.py
@@ -90,7 +90,3 @@ def uncomplete_todo(todo_id):
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({'error': 'Not found'}), 404
-
-@app.errorhandler(500)
-def internal_error(error):
-    return jsonify({'error': 'Internal server error'}), 500
